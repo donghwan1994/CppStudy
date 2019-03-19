996167953edae400f6380ee3f0c63a13816b436d
donghwan1994 <donghwan1994@naver.com>
2019-03-19 14:39:49 +0900

    1.8 예제코드

---
+++ Chapter1_1/Chapter1_8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x = 2; // x is a variable, 2 is a literal. 
+
+	int y = (x > 0) ? 1 : 2; // if x is -1 , y = ?
+	
+	cout << y << endl; 
+
+	return 0;
+}

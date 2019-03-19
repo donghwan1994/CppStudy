390c9fd0302620f9cb31f3c37e9ec8f642757c2e
donghwan1994 <donghwan1994@naver.com>
2019-03-19 15:13:01 +0900

    1.11 헤더파일 만들기 예제

---
+++ Chapter1_1/Chapter1_11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MyHeaders/add.h"
+#include "MyHeaders/subtract.h"
+
+using namespace std;
+
+int main()
+{
+	cout << add(1, 2) << endl;
+	cout << subtract(2, 1) << endl;
+
+	return 0;
+}

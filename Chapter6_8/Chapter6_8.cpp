4d052e229309100ba11ee7e998fe1eb87c20374c
donghwan1994 <donghwan1994@naver.com>
2019-04-15 18:07:05 +0900

    6.8 포인터와 정적 배열

---
+++ Chapter6_8/Chapter6_8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct MyStruct
+{
+	int array[5] = { 9, 7, 5, 3, 1 };
+};
+
+void doSomething(MyStruct *ms)
+{
+	cout << sizeof((*ms).array) << endl;
+}
+
+int main()
+{
+	MyStruct ms;
+	cout << ms.array[0] << endl;
+	cout << sizeof(ms.array) << endl;
+
+	doSomething(&ms);
+
+	return 0;
+}

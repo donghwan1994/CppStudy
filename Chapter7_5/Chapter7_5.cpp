518beeaa64fd640ffff4ff77cc27d40f51a9e3aa
donghwan1994 <donghwan1994@naver.com>
2019-04-22 15:48:54 +0900

    7.5 다양한 반환 값들(튜플)

---
+++ Chapter7_5/Chapter7_5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <tuple>
+
+using namespace std;
+
+std::tuple<int, double> getTuple()
+{
+	int a = 10;
+	double d = 3.14;
+	return std::make_tuple(a, d);
+}
+
+int main()
+{
+	std::tuple<int, double> my_tp = getTuple();
+	cout << std::get<0>(my_tp) << endl;
+	cout << std::get<1>(my_tp) << endl;
+
+	auto[a, d] = getTuple();
+	cout << a << endl;
+	cout << d << endl;
+
+	return 0;
+}

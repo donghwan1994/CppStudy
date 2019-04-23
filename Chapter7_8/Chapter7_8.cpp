35a15e8c5562a8ade4d82268053c274ac85a59e0
donghwan1994 <donghwan1994@naver.com>
2019-04-23 20:26:41 +0900

    7.8 매개변수의 기본값

---
+++ Chapter7_8/Chapter7_8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//void print(int x = 10, int y = 20, int z = 30)
+//{
+//	cout << x << " " << y << " " << z << endl;
+//}
+
+void print(std::string str) {}
+void print(char ch = ' ') {}
+
+int main()
+{
+	/*print();				// 10 20 30
+	print(100);				// 100 20 30
+	print(100, 200);		// 100 200 30
+	print(100, 200, 300);	// 100 200 300 */
+
+
+	//print(); // Error
+
+	return 0;
+}

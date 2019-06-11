92bfc85ad00bc21bd95096f4ef9c891e7f872435
donghwan1994 <donghwan1994@naver.com>
2019-06-11 11:10:54 +0900

    8.12 친구 함수와 클래스 friend

---
+++ Chapter8_12/Chapter8_12.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class B; // forward declaration;
+
+class A
+{
+private:
+	int m_value = 1;
+
+	friend void doSomething(A& a, B& b);
+};
+
+class B
+{
+private:
+	int m_value = 2;
+
+	friend void doSomething(A& a, B& b);
+};
+
+void doSomething(A& a, B& b)
+{
+	cout << a.m_value << "" << b.m_value << endl;
+}
+
+int main()
+{
+	A a;
+	doSomething(a);
+
+	return 0;
+}

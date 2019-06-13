7994c0e33936b7a88b7c616ed01adb82d86d59f9
donghwan1994 <donghwan1994@naver.com>
2019-06-13 12:20:02 +0900

    9.5 증감 연산자 오버로딩 하기

---
+++ Chapter9_05/Chapter9_05.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Digit
+{
+private:
+	int m_digit;
+
+public:
+	Digit(int digit = 0) : m_digit(digit) {}
+
+	//prefix
+	Digit & operator ++()
+	{
+		++m_digit;
+
+		return *this;
+	}
+
+	//postfix
+	Digit operator ++(int)
+	{
+		Digit temp(m_digit);
+		++*(this);
+
+		return temp.m_digit;
+	}
+
+	friend ostream & operator << (ostream &out, const Digit &d)
+	{
+		cout << d.m_digit;
+		return out;
+	}
+};
+
+int main()
+{
+	Digit d(5);
+
+	cout << ++d << endl;
+	cout << d++ << endl;
+	cout << d << endl;
+}
